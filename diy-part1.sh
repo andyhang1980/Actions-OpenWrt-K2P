f60c696424bd10daab04ed258ea3b777978da1ab
andy zhang <63940134+andyhang1980@users.noreply.github.com>
2023-11-18 22:38:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,8 @@
 
 # Add helloworld :D
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
+＃src-git kenzo https://github.com/kenzok8/openwrt-packages
+＃src-git small https://github.com/kenzok8/small
+＃src-git haibo https://github.com/haiibo/openwrt-packages
+＃src-git liuran001 https://github.com/liuran001/openwrt-packages
